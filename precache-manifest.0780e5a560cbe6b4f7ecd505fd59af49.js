68bce693f27fc9af75495d9bc0efd28f63e9adf3
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2019-01-26 01:59:53 +0530

    Updates

---
+++ precache-manifest.0780e5a560cbe6b4f7ecd505fd59af49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b5daf5dd0c05ad987b53029a77ef43",
+    "url": "/GitRegistry-ReactJS/static/media/gidolinya.a5b5daf5.woff"
+  },
+  {
+    "revision": "c85b57dfec730b15add4",
+    "url": "/GitRegistry-ReactJS/static/js/runtime~main.c85b57df.js"
+  },
+  {
+    "revision": "8185218f0f4ccc8a3705",
+    "url": "/GitRegistry-ReactJS/static/js/main.8185218f.chunk.js"
+  },
+  {
+    "revision": "f430ad69d8b1d117d9af",
+    "url": "/GitRegistry-ReactJS/static/js/1.f430ad69.chunk.js"
+  },
+  {
+    "revision": "8185218f0f4ccc8a3705",
+    "url": "/GitRegistry-ReactJS/static/css/main.607de4ed.chunk.css"
+  },
+  {
+    "revision": "f430ad69d8b1d117d9af",
+    "url": "/GitRegistry-ReactJS/static/css/1.a472519a.chunk.css"
+  },
+  {
+    "revision": "db82a3b777ab86dd5ec5b47c42667081",
+    "url": "/GitRegistry-ReactJS/index.html"
+  }
+];
