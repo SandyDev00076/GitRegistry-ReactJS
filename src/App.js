95af97c58888d2c90513614fce849c44c56c6ad4
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2019-02-20 03:37:26 +0530

    Fixed the build bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, HashRouter } from 'react-router-dom';
 import UserEntry from './components/UserEntryPage';
 import UserDetails from './components/UserDetailsPage';
 import './App.css';
@@ -7,12 +7,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={UserEntry}/>
-          <Route path={`${process.env.PUBLIC_URL}/users/:handle`} component={UserDetails}/>
+          <Route path={`/users/:handle`} component={UserDetails}/>
+          <Route exact path={``} component={UserEntry}/>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
